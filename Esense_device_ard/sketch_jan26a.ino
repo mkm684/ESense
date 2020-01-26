a3cdb40e151a132017e7ac8d376ba9b52f525c25
mkm684 <mkm684@mun.ca>
2020-01-26 04:05:00 -0330

    init commit

---
+++ Esense_device_ard/sketch_jan26a.ino
@@ -0,0 +1,63 @@
+#define USE_ARDUINO_INTERRUPTS true //set up low level interrupts for most accruate BPM math
+# include <PulseSensorPlayground.h> //includes the pulsesensor playground library
+ 
+ 
+int sensePin = A0;  //This is the Arduino Pin that will read the sensor output
+int sensorInput;    //The variable we will use to store the sensor input
+double temp;        //The variable we will use to store temperature in degrees celcius. High body temperatures are 40+, sensor accuracry is +/- 2 degrees 
+
+int beatPin=A2; //programming the pulse sensor 
+int beatInput;
+int Threshold= 550; //Determine which signal to count as a beat and which to ignore 
+PulseSensorPlayground pulseSensor; //creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+//averaging
+double temp_avg = 0;
+//double pres_avg = 0;  
+int samples = 0;
+
+//communication
+char dataString[50] = {0};
+
+
+void setup() {
+    pinMode(A0, INPUT);
+    pinMode(A2, INPUT);
+    // put your setup code here, to run once:
+    Serial.begin(9600); //Start the Serial Port at 9600 baud (default)
+    //configure the Pulsensor Object by assigning variables to it 
+    pulseSensor.analogInput(A2);
+    pulseSensor.setThreshold(Threshold);
+}
+ void loop() {
+    // The temperature
+    sensorInput=analogRead(A0);
+    temp=(double)sensorInput / 1024;       //find percentage of input reading
+    temp=temp*5;
+    temp=temp-0.5;
+    temp=temp*100; 
+    
+    //averaging
+    if (samples < 20){
+      temp_avg += temp;
+      samples++;
+    }
+    else {
+      int t = temp_avg/20;
+      sprintf(dataString,"%02X",(int)t); // convert a value to hexa 
+      Serial.println(dataString);
+      temp_avg=0;
+      samples=0;
+    }
+
+    //Pulse sensor
+//    int myBPM=pulseSensor.getBeatsPerMinute(); //Calls function on our pulse sensor 
+//    if (pulseSensor.sawStartOfBeat())
+//    { //Constantly test to see if a beat "happened"
+//      Serial.println(myBPM);
+//    }
+//    Serial.print("HeatBeat") ;  
+//    Serial.println(beatInput);  
+    
+    delay(50); //wait fifty millisecond 
+}
---
+++ flask-api/api.py
@@ -0,0 +1,43 @@
+import flask
+from flask import request, jsonify
+import requests
+import json
+import serial
+import threading
+import re
+class temp(threading.Thread):
+	def __init__(self):
+	    threading.Thread.__init__(self)
+	    self.latest_temp = 10
+	    try:
+	    	self.ser = serial.Serial('/dev/ttyACM0',9600)
+	    except  Exception as e:
+	    	self.ser = serial.Serial('/dev/ttyACM1',9600)
+	    finally:
+	    	print("connection to Esense device failed")
+
+	def run(self):
+		while True:
+			ser_out = self.ser.readline()
+			ser_out_char = re.findall(r'\d+', ser_out)
+			ser_out_int = [int(s) for s in ser_out_char[0].split() if s.isdigit()]
+			self.latest_temp = ser_out_int[0]
+
+
+temp_obj = temp()
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/result', methods = ['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+    	return jsonify(temp_obj.latest_temp)
+
+temp_obj.start()
+app.run()
+
+
